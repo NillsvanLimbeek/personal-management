dc635e06d620732d8509005f62b82fbc961944ba
Nills van Limbeek <nills.van.limbeek@live.nl>
2019-06-21 21:52:29 +0200

    first commit

---
+++ pm-spa/src/components/datepicker/Datepicker.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="datepicker">
+
+    </div>
+</template>
+
+<script>
+    import { Vue, Component } from '@/vue-script';
+
+    export default class Datepicker extends Vue {}
+</script>
