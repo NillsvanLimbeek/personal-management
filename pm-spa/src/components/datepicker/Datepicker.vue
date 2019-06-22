9ded99b1456badba15f09c59acd16cf3c8c321f3
Nills van Limbeek <nills.van.limbeek@live.nl>
2019-06-22 11:20:11 +0200

    moved to datepicker component

--- pm-spa/src/components/datepicker/Datepicker.vue
+++ pm-spa/src/components/datepicker/Datepicker.vue
@@ -1,11 +1,81 @@
 <template>
     <div class="datepicker">
+        <button @click="previousMonth">
+            previous month
+        </button>
 
+        {{ months[1][0] }}
+
+        <button @click="nextMonth">
+            next month
+        </button>
     </div>
 </template>
 
-<script>
-    import { Vue, Component } from '@/vue-script';
+<script lang="ts">
+    import { Vue, Component, Prop } from '@/vue-script';
 
-    export default class Datepicker extends Vue {}
+    import {
+        startOfMonth,
+        getDaysInMonth,
+        addDays,
+        format,
+        subMonths,
+        addMonths,
+    } from 'date-fns';
+
+    @Component({})
+    export default class Datepicker extends Vue {
+        @Prop() private date!: Date;
+
+        private months: any[] = [];
+        private startDates: Date[] = [];
+
+        private getDates(date?: Date) {
+            let currentMonth: Date;
+
+            if (date) {
+                currentMonth = date;
+            } else {
+                currentMonth = startOfMonth(Date.now());
+            }
+
+            const previousMonth: Date = subMonths(currentMonth, 1);
+            const nextMonth: Date = addMonths(currentMonth, 1);
+
+            this.startDates = [previousMonth, currentMonth, nextMonth];
+        }
+
+        private getDays(date: Date) {
+            let month: Date[];
+
+            month = [...Array(getDaysInMonth(date))].map((y, x) => {
+                return addDays(date, x);
+            });
+
+            return month;
+        }
+
+        private previousMonth() {
+            const previousMonth = subMonths(this.months[0][0], 1);
+
+            this.months.pop();
+            this.months.unshift(this.getDays(previousMonth));
+        }
+
+        private nextMonth() {
+            const nextMonth = addMonths(this.months[2][0], 1);
+
+            this.months.shift();
+            this.months.push(this.getDays(nextMonth));
+        }
+
+        private created() {
+            this.date ? this.getDates(this.date) : this.getDates();
+
+            this.startDates.forEach((date: Date) => {
+                this.months.push(this.getDays(date));
+            });
+        }
+    }
 </script>
--- pm-spa/src/views/tasks/TaskCalendarSection.vue
+++ pm-spa/src/views/tasks/TaskCalendarSection.vue
@@ -2,21 +2,13 @@
     <div class="calendar">
         <h2>Calendar</h2>
 
-        <Datepicker />
+        <Datepicker :date="nextYear" />
     </div>
 </template>
 
 <script lang="ts">
     import { Vue, Component } from '@/vue-script';
-
-    import {
-        startOfMonth,
-        getDaysInMonth,
-        addDays,
-        format,
-        subMonths,
-        addMonths,
-    } from 'date-fns';
+    import { startOfMonth, addYears } from 'date-fns';
 
     const Datepicker = () => import('@components/datepicker/Datepicker.vue');
 
@@ -26,28 +18,7 @@
         },
     })
     export default class TaskCalendarSection extends Vue {
-        private months: any[] = [];
-        private startDates: Date[] = [];
-
-        private getDays() {
-            let month: Date[];
-            const today: Date = startOfMonth(Date.now());
-
-            month = [...Array(getDaysInMonth(today))].map((y, x) => {
-                return addDays(today, x);
-            });
-
-            return month;
-        }
-
-        private getStartDates() {
-            const today: Date = startOfMonth(Date.now());
-            const previousMonth: Date = subMonths(today, 1);
-            const nextMonth: Date = addMonths(today, 1);
-        }
-
-        private created() {
-            this.months.push(this.getDays());
-        }
+        private today: Date = startOfMonth(Date.now());
+        private nextYear: Date = addYears(this.today, 1);
     }
 </script>
