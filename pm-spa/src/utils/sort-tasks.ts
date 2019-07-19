0490de08174a4f7a63c0809a65f4fffa6c883b1c
Nills van Limbeek <n.vanlimbeek@timechimp.com>
2019-07-19 06:39:40 +0200

    updated sort tasks util

--- pm-spa/src/utils/sort-tasks.ts
+++ pm-spa/src/utils/sort-tasks.ts
@@ -1,22 +1,18 @@
 import { ITask } from './../data/models/task';
 
-// function compareNumbers(a: string, b: string) {
-//     return a - b;
-// }
-
-export function sortTasks(arr: ITask[]) {
-    const sorted = arr.sort((a, b) => {
+export function sortTasks(arr: ITask[]): ITask[] {
+    return arr.sort((a, b) => {
         const nameA = a.title.toUpperCase();
         const nameB = b.title.toUpperCase();
+
         if (nameA < nameB) {
             return -1;
         }
+
         if (nameA > nameB) {
             return 1;
         }
 
         return 0;
     });
-
-    return sorted;
 }
