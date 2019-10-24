dfe39122a0552f818b1c4ed81079b3b80e1ddf29
NillsvanLimbeek <nills.van.limbeek@live.nl>
2019-10-24 08:17:24 +0200

    button test

--- pm-spa/src/components/button/__tests__/Button.spec.ts
+++ pm-spa/src/components/button/__tests__/Button.spec.ts
@@ -3,11 +3,17 @@ import { shallowMount } from '@vue/test-utils';
 import Button from '../Button.vue';
 
 describe('Button', () => {
-    const wrapper = shallowMount(Button);
+    const wrapper = shallowMount(Button, {
+        slots: {
+            default: '<p class="slot"></p>',
+        },
+    });
 
     it('Renders a button', () => {
         expect(wrapper.contains('.button')).toBe(true);
     });
 
-    // TODO test for slot
+    it('Renders a slot', () => {
+        expect(wrapper.find('.slot')).toBeTruthy();
+    });
 });
