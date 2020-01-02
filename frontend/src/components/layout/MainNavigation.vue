3cfdfcbb019dce4326b04e12eedc9a372b761a1b
Nills <nills.van.limbeek@live.nl>
2020-01-02 16:56:58 +0100

    task sections widget fixes

--- frontend/src/components/layout/MainNavigation.vue
+++ frontend/src/components/layout/MainNavigation.vue
@@ -1,23 +1,17 @@
 <template>
     <div class="main-navigation">
         <div class="main-navigation__logo">
-            Project
+            Personal
             <span class="title-bold">Management</span>
         </div>
 
         <div class="main-navigation__links">
-            <router-link
-                :to="{ name: 'home' }"
-                class="main-navigation__link"
-            >
+            <router-link :to="{ name: 'home' }" class="main-navigation__link">
                 <i class="fas fa-home" />
                 Home
             </router-link>
 
-            <router-link
-                :to="{ name: 'tasks' }"
-                class="main-navigation__link"
-            >
+            <router-link :to="{ name: 'tasks' }" class="main-navigation__link">
                 <i class="far fa-check-circle" />
                 Tasks
             </router-link>
@@ -26,11 +20,10 @@
 </template>
 
 <script lang="ts">
-    import { Vue, Component, Prop } from '@/vue-script';
+import { Vue, Component, Prop } from '@/vue-script';
 
-    @Component({})
-    export default class MainNavigation extends Vue {}
+@Component({})
+export default class MainNavigation extends Vue {}
 </script>
 
-<style lang="scss" src="./MainNavigation.scss">
-</style>
+<style lang="scss" src="./MainNavigation.scss"></style>
